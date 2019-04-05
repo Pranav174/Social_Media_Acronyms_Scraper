2a0a12e36c0dc0b4589f8df9018febb0bb776f43
Pranav174 <pranav.goyal@research.iiit.ac.in>
2019-04-05 21:53:14 +0530

    Made list of multiple expansions

--- MultipleExpansions.cpp
+++ MultipleExpansions.cpp
@@ -2,28 +2,32 @@
 #include <string.h>
 using namespace std;
 typedef long long int ll;
+
 int main()
 {
     char x;
     string t1, prev;
-    string t2;
+    string t2, prevt2;
     cin >> prev;
     x = getchar();
     while (x = getchar())
     {
         if (x == '\n' || x == '\0')
         {
-            cout << prev << endl;
-            cout << "1: " << t2 << endl;
+            /* cout << prev << endl;
+            cout << "1: " << t2 << endl;*/
             break;
         }
         else
             t2.push_back(x);
     }
+    ll index = 1;
     ll count = 1;
-    ll ans =0;
+    //ll ans = 0;
+    //cout << "{";
     while (cin >> t1)
     {
+        prevt2 = t2;
         t2.clear();
         x = getchar();
         while (x = getchar())
@@ -32,19 +36,23 @@ int main()
             {
                 if (prev == t1)
                 {
+                    cout << index << ": " << prevt2 << endl;
                     count++;
-                    cout << count << ": " << t2 << endl;
+                    index++;
                 }
                 else
                 {
-                    if (count>1){
-                        ans++;
+                    if (count > 1)
+                    {
+                        cout << index << ": " << prevt2 << endl;
+                        index++;
+                        //ans++;
                     }
-                    cout << endl;
+                    //cout << endl;
                     prev = t1;
                     count = 1;
-                    cout << t1 << endl;
-                    cout << count << ": " << t2 << endl;
+                    //cout << t1 << endl;
+                    //cout << count << ": " << t2 << endl;
                 }
                 break;
             }
@@ -52,6 +60,7 @@ int main()
                 t2.push_back(x);
         }
     }
-    cout << endl << "Multiple expansions: "<< ans << endl;
+    //cout << "}";
+    //cout << ans << endl;
     return 0;
 }
